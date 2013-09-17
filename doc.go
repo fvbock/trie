e79b67463003bc8c721f22fb001a0f68a80dd59b
Florian von Bock <f@vonbock.info>
2013-09-18 01:32:00 +0900

    initial commit

---
+++ doc.go
@@ -0,0 +1,14 @@
+/*
+Trie is a prefix index package for golang.
+
+Terminology used
+
+Trie - Contains the Root of the Index - which is a Branch
+
+Branch - A Branch might have a LeafValue or not and might have other Branches splitting off.
+
+Entry - an entry refers to a _complete_ term that is inserted, removed from, or matched in the index. It requires `End` on the Branch to be set to `true`, which makes it different from a
+
+Prefix - which does not require the Branch to have End set to `true` to match.
+*/
+package trie
